913ef96595623515308cb7eab2de44e492489c16
Matthew Sewell <mattfsewell@gmail.com>
2020-11-22 18:51:34 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,4 @@
 def find_element_index(array, value_to_find)
-  for i in 0...array.length
-    if array[i] == array[value_to_find]
-      return i
-    end
-  end
+  if array.include?(value_to_find) == true
+    
 end
